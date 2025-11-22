195bb3878dd1815bc10504726cef33b1fb276621
sothengski <chheangsotheng@gmail.com>
2025-11-22 00:24:26 -0800

    fix(refactor): refactor providers for singleton

--- lib/features/auth/presentation/signin/custom_signin_screen.dart
+++ lib/features/auth/presentation/signin/custom_signin_screen.dart
@@ -1,30 +1,27 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_auth_todo/core/routes/app_router.dart';
+import 'package:flutter_firebase_auth_todo/features/auth/presentation/ui_auth_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authProvider = <AuthProvider>[
-      EmailAuthProvider(),
-      // PhoneAuthProvider(),
-    ];
+    final authProviders = ref.watch(authProvidersProvider);
+
     return Scaffold(
       appBar: AppBar(title: Text('Sign In')),
       body: SignInScreen(
-        providers: authProvider,
-        actions: [
-          AuthStateChangeAction<SignedIn>((context, state) {
-            context.goNamed(AppRoute.profile.name);
-          }),
-          AuthStateChangeAction<UserCreated>((context, state) {
-            context.goNamed(AppRoute.profile.name);
-          }),
-        ],
+        providers: authProviders,
+        // actions: [
+        //   AuthStateChangeAction<SignedIn>((context, state) {
+        //     context.goNamed(AppRoute.profile.name);
+        //   }),
+        //   AuthStateChangeAction<UserCreated>((context, state) {
+        //     context.goNamed(AppRoute.profile.name);
+        //   }),
+        // ],
       ),
     );
   }
